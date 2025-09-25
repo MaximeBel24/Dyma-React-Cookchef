cfa6699da4c7c489efaf9c129e02fb526940e0e9
Maxime Belin <maxime.dev9424@gmail.com>
2025-09-25 17:07:54 +0200

    chargement des articles et context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './assets/styles/index.scss'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './assets/styles/index.scss';
+import App from './App';
+import { ApiContext } from './context/ApiContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <ApiContext.Provider value="https://restapi.fr/api/recipes">
+            <App />
+        </ApiContext.Provider>
+    </StrictMode>
+);
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,37 @@
 import styles from './HomePage.module.scss';
 import Recipe from './components/Recipe/Recipe.jsx';
-import {useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import Loading from "../../components/Loading/Loading.jsx";
+import {ApiContext} from "../../context/ApiContext.jsx";
 
 function HomePage() {
 
     const [recipes, setRecipes] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [filter, setFilter] = useState('');
+    const BASE_URL_API = useContext(ApiContext);
+
+    useEffect(() => {
+        let cancel = false;
+        async function fetchRecipes() {
+            try{
+                setIsLoading(true);
+                const response = await fetch(BASE_URL_API);
+                if (response.ok && !cancel) {
+                    const recipes = await response.json();
+                    setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+                }
+            } catch (e) {
+                console.log('ERREUR : ', e);
+            } finally {
+                if (!cancel) {
+                    setIsLoading(false);
+                }
+            }
+        }
+        fetchRecipes();
+        return () => (cancel = true);
+    }, [BASE_URL_API]);
 
     function handleInput(e) {
         const filter = e.target.value;
@@ -15,7 +39,7 @@ function HomePage() {
     }
 
     return (
-        <div className="flex-fill container p-20">
+        <div className="flex-fill container d-flex flex-column p-20">
             <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
             <div className={`card p-20 flex-column p-20 ${styles.contentCard}`}>
                 <div className={`d-flex flex-row justify-content-center align-items-center my-30 ${styles.searchBar}`}>
